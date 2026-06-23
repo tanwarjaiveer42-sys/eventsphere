9537ccaacea78acc7e317d8daa28e03dafba5cb6
Jaiveer Tanwar <tanwarjaiveer42@gmail.com>
2026-06-24 00:41:23 +0530

    Connect backend with MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
 const express = require("express");
 
 const cors = require("cors");
@@ -8,13 +12,15 @@ app.use(cors());
 
 app.use(express.json());
 
+connectDB();
+
 app.get("/", (req,res)=>{
 
 res.send("EventSphere Backend Running");
 
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
